ea299bf21c97d098a703881014fe0aff14ae6fa4
Gejza Jenča <gejza.jenca@stuba.sk>
2023-03-21 11:22:05 +0100

    prisposobene pre nove podmienky

--- README.md
+++ README.md
@@ -1,14 +1,15 @@
-# Testovanie mboxer servera
+# Testovanie reader servera
 
 ## Čo urobiť pred testovaním
 
 1. Pred použitím musíte nainštalovať knižnicu `pyyaml`, ktorá sa možno volá vo Vašej distribúcii
    inak, možno `python3-yaml` alebo tak nejako.
 2. Takisto je treba nainštalovať pythonovskú knižnicu `pygments`.
+3. Musíte mať nainštalovanú aj knižnicu `ncurses`, ale tú takmer iste nainštalovanú máte.
 
 ## Ako testovať hromadne všetky testy
 
-1. Skopírujte do tohto adresára váš program a nazvite ho `mboxer.py`.
+1. Skopírujte do tohto adresára váš program a nazvite ho `reader.py`.
 2. Spustite skript `runtests.sh`.
 3. Spustia sa testy. Počkajte, kým dobehnú, pár sekúnd to trvá.
 4. Vznikne súbor `results.html`, ktorý si môžete otvoriť v browseri.
@@ -17,4 +18,4 @@
 
 ## Čo robiť ak niečo nejde
 
-Ak Vám to testovanie nejde, ozvite sa mi a zistíme, kde je problém.
+Ak Vám to testovanie nejde, ozvite sa mi buď emailom alebo napíšte issue do tohto projektu na githube zistíme, kde je problém.
--- runtests.sh
+++ runtests.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 # vim: et ts=4 sw=4 sta ai
-./getsource.sh
+
+PATTERN='.'
+
+WHICH=""
+if [ -n "$1" ]
+then
+    WHICH="_$1"
+fi
+
+./getsource.sh "$WHICH"
 (
 cat << THEEND
 <!DOCTYPE html>
@@ -32,12 +41,12 @@ TESTS=`echo test??`
 echo '<tr>'
 echo '<th>*</th>'
 for TEST in $TESTS; do
-    echo '<th>' `cat $TEST/test.name | sed 's/+/<br>/g'` '</th>'
+    echo '<th>' `(echo $TEST '+' ; cat $TEST/test.name )| sed 's/+/<br>/g'` '</th>'
     rm -r $TEST/results 2>/dev/null
     mkdir $TEST/results
 done
 echo '</tr>'
-./riesenia.sh $@ |
+./riesenia"$WHICH".sh 2>/dev/null | grep "$PATTERN" |
 while IFS=: read RIESENIE NAME ; do
     if [ "$NAME" == "" ]
     then
@@ -45,27 +54,32 @@ while IFS=: read RIESENIE NAME ; do
     fi
     ERR=results/test_${NAME}.err
     OUT=results/test_${NAME}.txt 
+    printf '%-20s' "${NAME}" > /dev/stderr
     echo '<tr>'
     echo '<th> <a href="source/'${NAME}'.html">'${NAME}'</a></th>'
     for TEST in $TESTS; do
         cd $TEST
         bash test.sh ../$RIESENIE 2>${ERR} >${OUT}
         EXIT_STATUS=$?
-        sleep 0.1
         echo '<td>'
         if [ "$EXIT_STATUS" == "0" ]
         then
             CLASS='passed'
             MARK='✓'
+            COLOR="$(tput setaf 2)"
         else
             CLASS='failed'
             MARK='X'
+            COLOR="$(tput setaf 1)"
         fi
+        printf "${COLOR}%s$(tput sgr0)" ${MARK} >/dev/stderr
         echo '<span class="'$CLASS'">'$MARK'</span>'
         echo '<a href="'$TEST/${OUT}'">&gt;&gt;</a>'
         if [ ! -s ${ERR} ]; then
+            printf '_' > /dev/stderr
 	    	rm ${ERR}
         else
+            printf '!' >/dev/stderr
             echo '<a href="'$TEST/${ERR}'">err&gt;&gt;</a>'
 	    fi
         echo '</td>'
@@ -73,6 +87,7 @@ while IFS=: read RIESENIE NAME ; do
         killall python 2>/dev/null
         killall python3 2>/dev/null
     done
+    printf '\n' > /dev/stderr
     echo '</tr>'
 done
 cat <<THEEND
@@ -80,5 +95,5 @@ cat <<THEEND
 </body>
 </html>
 THEEND
-) | tee results.html
+) > results.html
 
