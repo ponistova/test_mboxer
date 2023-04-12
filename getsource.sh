0d8c9f30933176ba68f4a0591abd2befd2191a89
Gejza Jenča <gejza.jenca@stuba.sk>
2023-04-12 09:47:53 +0200

    this

--- getsource.sh
+++ getsource.sh
@@ -1,18 +1,25 @@
 #!/bin/bash
 # vim: et ts=4 sw=4 sta ai
 
+WHICH="$1"
+
+if ! command -v pygmentize >/dev/null
+then
+    exit 0
+fi
 echo '>>> Converting source code'
-rm -r source 2>/dev/null
 mkdir -p source
-./riesenia.sh $@ |
+./riesenia"$WHICH".sh $@ |
 while IFS=: read RIESENIE NAME ; do
     if [ "$NAME" == "" ]
     then
         NAME="noname"
     fi
     OUT="source/${NAME}.html"
-    echo "$RIESENIE" '->' "$OUT"
-    pygmentize -l python -f html -O full -O linenos -o "$OUT" $RIESENIE
+    if [ \( ! -e "$OUT" \) -o \( "$RIESENIE" -nt "$OUT" \) ]; then
+        echo "$RIESENIE" '->' "$OUT" > /dev/stderr
+        pygmentize -l python -f html -O full -O linenos -o "$OUT" "$RIESENIE"
+    fi
 done
-
+echo '>>> Done'
 
--- test02/test.sh
+++ test02/test.sh
@@ -1,13 +1,1 @@
-killall python3 2>/dev/null
-rm -r xxx 2>/dev/null
-mkdir -p xxx
-( python3 "$1"  >/dev/null& )
-PID=$!
-sleep 0.2
-python3 test.py
-EXIT_STATUS=$?
-echo -------------
-killall python3 2>/dev/null
-echo -------------
-ls -l xxx
-exit $EXIT_STATUS
+../test.sh
